b7bbaa27968bacad35fa1a3f34010e8a9978a2cd
Dereck-Desjardins <dereck.desjardins@hotmail.com>
2024-10-03 15:49:56 -0400

    kormitt

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -8,37 +8,52 @@ export default class MathsController extends Controller {
     }
     get(id){
         let params = this.HttpContext.path.params;
-        let reponse = 0;
-        console.log(params);
-        switch(params.op){
-            case" ":
+        let singleParams = ["!", "p", "np"];
+        if(singleParams.includes(params.op)){
+            if(n != null || n != undefined){
+                switch(params.op){
+                    case"!":
+                        this.HttpContext.response.JSON(utilities.factorialize(params.n));
+                        break;
+                    case"p":
+                        let result = utilities.isPrime(params.n)
+                        this.HttpContext.response.JSON(result);
+                        break;
+                    case"np":
+                        this.HttpContext.response.JSON(utilities.posPrime(params.n));
+                        break;
+                    default:
+                        this.HttpContext.response.notFound();
+                        break;
+                }
+            }
+            else{
+                let reponse = "n parameter is not defined"
+                this.HttpContext.response.JSON()
+            }
+
+        }
+        else{
+            switch(params.op){
+                case" ":
                 this.HttpContext.response.JSON(params.x + params.y);
                 break;
-            case"-":
-                this.HttpContext.response.JSON(params.x - params.y);
-                break;
-            case"*":
-                this.HttpContext.response.JSON(params.x * params.y);  
-                break;          
-            case"/":
-                this.HttpContext.response.JSON(params.x / params.y);
-                break;
-            case"%":
-                this.HttpContext.response.JSON(params.x % params.y);
-                break;
-            case"!":
-                this.HttpContext.response.JSON(utilities.factorialize(params.x));
-                break;
-            case"p":
-                let result = utilities.isPrime(params.x)
-                this.HttpContext.response.JSON(result);
-                break;
-            case"np":
-                this.HttpContext.response.JSON(utilities.posPrime(params.x));
-                break;
-            default:
-                this.HttpContext.response.notFound();
-                break;
+                case"-":
+                    this.HttpContext.response.JSON(params.x - params.y);
+                    break;
+                case"*":
+                    this.HttpContext.response.JSON(params.x * params.y);  
+                    break;          
+                case"/":
+                    this.HttpContext.response.JSON(params.x / params.y);
+                    break;
+                case"%":
+                    this.HttpContext.response.JSON(params.x % params.y);
+                    break;
+                default:
+                    this.HttpContext.response.notFound();
+                    break;    
+            }
         }
     }
 }
